893b3a010d88c834a5833f47fc1ddc83ecce74c3
ECNUyunzhe <yunzhe.uk@outlook.com>
2026-02-14 16:28:51 +0000

    Deploying to gh-pages from @ ECNUyunzhe/ECNUyunzhe.github.io@968592ac14e305957d5ce3ff0fde427f806e61b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-home-主页",
+    title: "Home 主页",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
