44fda6db735dcad0be6f3cd64b8e6d113fc450ab
DAWN-ECNU <yunzhe.uk@outlook.com>
2026-02-15 10:34:44 +0000

    Deploying to gh-pages from @ DAWN-ECNU/DAWN-ECNU.github.io@8829e388431505505943e87ffce7b4c8ac59151d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,9 +436,9 @@ ninja.data = [{
           description: "Population Migration &amp; Urban Mobility",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-社会感知与数据挖掘",
+          title: '社会感知与数据挖掘',
+          description: "Social Sensing &amp; Data Mining",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
