7ea8be4d62864229008eef308459c3ed0cf15f52
DAWN-ECNU <yunzhe.uk@outlook.com>
2026-02-15 10:32:02 +0000

    Deploying to gh-pages from @ DAWN-ECNU/DAWN-ECNU.github.io@5e3ddcbfae556a78b5ed901f933da337883bc148 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,9 +426,9 @@ ninja.data = [{
           description: "Urban Resilience",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-社会空间分异",
+          title: '社会空间分异',
+          description: "Socio-spatial Disparities",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
